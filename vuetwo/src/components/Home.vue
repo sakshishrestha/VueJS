c7f98507f4d2cff24c209004291b18b8b105b122
Sakshi Shrestha <sakshi@8squarei.com>
2023-01-25 09:59:27 +0545

    Add vuetwo with vue 2.7 version with Vue CLI

---
+++ vuetwo/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Home Component</h1>
+        <h2>{{ data }}</h2>
+        <!--v-on click event in vuejs-->
+        <button v-on:click="hello('hello call')">Call Hello Function</button> 
+        <!--v-if v-else conditional rendering-->
+        <h1 v-if="show">Conditional Rendering If block </h1>
+        <h1 v-else>Conditional Rendering Else block</h1>
+        <button v-on:click="display">Toogle ELement</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home',
+    props:{
+        data:String
+    },data(){
+        return {show:true}
+    },methods:{
+        hello(data){
+            alert(data)
+        },
+        display(){
+            this.show=!this.show
+        }
+    }
+}
+</script>
